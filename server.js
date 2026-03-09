4fa3e12cea2a9d1818e4ea344f1316aabbdc7363
praveenshnoor <praveen@shnoor.com>
2026-03-09 16:48:23 +0530

    CORS issue resolved

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ const allowedSocketOrigins = [
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:5174'],
+        origin: allowedSocketOrigins,
         methods: ["GET", "POST"],
-        credentials: false
+        credentials: true
     },
     pingTimeout: 60000,
     pingInterval: 25000,
